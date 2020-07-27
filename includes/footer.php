292915b2f6df54940d6d22cf4943802a7e8aa4a0
Willian Danilo <55411079+willdanilomendes@users.noreply.github.com>
2020-07-27 10:35:25 -0300

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+   <!-- SCRIPTS JS -->
+   <script src="<?php echo U?>src/vendors/bootstrap/bootstrap.bundle.min.js"></script>
+   <script src="<?php echo U?>src/vendors/jquery/jquery-3.3.1.min.js"></script>
+   <script src="<?php echo U?>src/vendors/jquery/jquery.mask.min.js"></script>
+   <!-- SCRIPTS JS -->
+
+   </body>
+
+   </html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- INCLUDE -->
+<?php
+  define('U', '');
+  include_once(U . 'includes/header.php');
+?>
+<!-- INCLUDE -->
+
+<!-- Título -->
+<title>Título</title>
+
+</head>
+
+<body>
+
+  <main>
+
+  </main>
+
+  <?php include_once(U . 'includes/footer.php'); ?>
